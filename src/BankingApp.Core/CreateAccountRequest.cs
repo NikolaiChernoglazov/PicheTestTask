7fe32b0cea1f294039af1b50cc7b26709f909a65
Nikolai Chernoglazov <nikolai.i.chernoglazov@gmail.com>
2024-11-24 20:15:53 +0200

    fix: Fix CreateAccountRequest

--- src/BankingApp.Core/CreateAccountRequest.cs
+++ src/BankingApp.Core/CreateAccountRequest.cs
@@ -5,20 +5,20 @@ namespace BankingApp.Core;
 /// <summary>
 /// CreateAccountRequest
 /// </summary>
-public record CreateAccountRequest(
-    string Currency,
-    decimal Amount)
+public class CreateAccountRequest(
+    string currency,
+    decimal amount)
 {
     /// <summary>
     /// Code of currency to open an account in.
     /// </summary>
     /// <example>USD</example>
     [Required]
-    public string Currency { get; init; } = Currency;
+    public string Currency { get; init; } = currency;
     
     /// <summary>
     /// The initial amount.
     /// </summary>
     /// <example>100.0</example>
-    public decimal Amount { get; init; } = Amount;
+    public decimal Amount { get; init; } = amount;
 }
